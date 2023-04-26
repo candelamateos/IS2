c9d38769fab33ed398623a056df14297d47df8fc
candelamateos <candela14agosto@gmail.com>
2023-04-26 18:57:53 +0200

    DAO CLIENTE

--- AgenciaDeViajes/src/integracion/cliente/DaoCliente.java
+++ AgenciaDeViajes/src/integracion/cliente/DaoCliente.java
@@ -1,5 +1,18 @@
 package integracion.cliente;
 
-public interface DaoCliente {
+import java.util.List;
 
+import negocio.cliente.TCliente;
+
+public interface DaoCliente {
+	
+public int createCliente(TCliente cliente);
+	
+	public int updateCliente(TCliente cliente);
+	
+	public int deleteCliente(TCliente cliente);
+	
+	public TCliente readCliente(TCliente cliente);
+	
+	public List<TCliente> readAllCliente(TCliente cliente);
 }
--- AgenciaDeViajes/src/integracion/cliente/DaoClienteImp.java
+++ AgenciaDeViajes/src/integracion/cliente/DaoClienteImp.java
@@ -1,5 +1,39 @@
 package integracion.cliente;
 
-public class DaoClienteImp {
+import java.util.List;
+
+import negocio.cliente.TCliente;
+
+public class DaoClienteImp implements DaoCliente {
+
+	@Override
+	public int createCliente(TCliente cliente) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateCliente(TCliente cliente) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteCliente(TCliente cliente) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public TCliente readCliente(TCliente cliente) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TCliente> readAllCliente(TCliente cliente) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
