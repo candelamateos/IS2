1ce3a1fd12fb9174956688bb3931f3d5fe3d9f06
rsangil03 <131896527+rsangil03@users.noreply.github.com>
2023-04-26 20:22:52 +0200

    dao cliente

--- AgenciaDeViajes/src/integracion/cliente/DaoClienteImp.java
+++ AgenciaDeViajes/src/integracion/cliente/DaoClienteImp.java
@@ -1,8 +1,15 @@
 package integracion.cliente;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.List;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import negocio.cliente.TCliente;
 
@@ -10,13 +17,15 @@ import negocio.cliente.TCliente;
 
 public class DaoClienteImp implements DaoCliente {
 
+	private static final String ARCHIVO = "clientes.json";
+	
 	@Override
 	public int createCliente(TCliente cliente) {
 		int id = -1;
 		JSONObject json = new JSONObject();
 		
 		json.put("nombre", cliente.getNombre());
-		json.put("activo", cliente.getActivo());
+		json.put("activo", true);
 		
 		// TODO Auto-generated method stub
 		return id;
@@ -44,6 +53,21 @@ public class DaoClienteImp implements DaoCliente {
 	public List<TCliente> readAllCliente(TCliente cliente) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	private JSONObject loadData() {
+		InputStream input;
+		JSONObject jsonInput;
+		try {
+			input = new FileInputStream(new File(ARCHIVO));
+			jsonInput = new JSONObject(new JSONTokener(input));
+		} catch (FileNotFoundException e) {
+			jsonInput = new JSONObject();
+			jsonInput.put("Ultimo id", 0);
+			jsonInput.put("Clientes", new JSONArray());
+		}
+		
+		return jsonInput;
 	}
 
 }
