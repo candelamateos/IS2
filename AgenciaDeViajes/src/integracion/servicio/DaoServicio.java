500019b7c2cb0c6cfceeec91e0bb5c5201a12036
jaimixu1 <jaimer08@ucm.es>
2023-04-27 14:20:26 +0200

    pasddas

--- AgenciaDeViajes/src/integracion/servicio/DaoServicio.java
+++ AgenciaDeViajes/src/integracion/servicio/DaoServicio.java
@@ -1,5 +1,16 @@
 package integracion.servicio;
 
-public interface DaoServicio {
+import java.util.List;
 
+
+public interface DaoServicio {
+	public int altaServicio(TServicio servicio);
+	
+	public int bajaServicio(TServicio servicio);
+	
+	public int modificarServicio(TServicio servicio);
+	
+	public TServicio buscarServicio(ID id);
+	
+	public List<TServicio> listarServicio();
 }
