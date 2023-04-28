26ec85de89f373ac224fe0a8cb48905456b88b8c
candelamateos <candela14agosto@gmail.com>
2023-04-28 18:14:43 +0200

    PRESENTACION CLIENTES

--- AgenciaDeViajes/src/presentacion/cliente/VistaListarCliente.java
+++ AgenciaDeViajes/src/presentacion/cliente/VistaListarCliente.java
@@ -1,5 +1,42 @@
 package presentacion.cliente;
 
-public class VistaListarCliente {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import presentacion.IGUI;
+import presentacion.controlador.Controlador;
+import presentacion.controlador.Eventos;
+
+public class VistaListarCliente extends JFrame implements IGUI {
+
+	JLabel lId;
+	JTextField tId;
+	JButton ok;
+	public VistaListarCliente() {
+		setTitle("LISTAR CLIENTE");
+		ok.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				Controlador.getInstancia().accion(Eventos.LISTAR_CLIENTE, null);
+			}
+		});
+	}
+
+	
+	@Override
+	public void actualizar(int evento, Object datos) {
+		// TODO Auto-generated method stub
+		
+	} 
+
+	
+	
 }
