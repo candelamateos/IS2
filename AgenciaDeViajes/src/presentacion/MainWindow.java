bbbe390d3743525e3fc7522985ae8cf68432ed2b
rsangil03 <131896527+rsangil03@users.noreply.github.com>
2023-04-29 12:52:48 +0200

    MainWindow añadida

---
+++ AgenciaDeViajes/src/presentacion/MainWindow.java
@@ -0,0 +1,11 @@
+package presentacion;
+
+public class MainWindow implements IGUI {
+
+	@Override
+	public void actualizar(int evento, Object datos) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
