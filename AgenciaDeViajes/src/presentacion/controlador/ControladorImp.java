e4626f5586258e86ada3f264cfb79599d6b45426
rsangil03 <131896527+rsangil03@users.noreply.github.com>
2023-05-01 17:21:43 +0200

    ss

--- AgenciaDeViajes/src/presentacion/controlador/ControladorImp.java
+++ AgenciaDeViajes/src/presentacion/controlador/ControladorImp.java
@@ -16,6 +16,8 @@ import negocio.viaje.TViaje;
 import presentacion.factoria.FactoriaAbstractaPresentacion;
 
 public class ControladorImp extends Controlador {
+	
+	
 	public void accion(int evento, Object datos) {
 		switch (evento) {
 		
